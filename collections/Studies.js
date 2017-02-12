adcb1bdd0e5e13abdafcee7989c9be9d138920a0
Toni Kleemola <toni.kleemola@student.oulu.fi>
2017-02-12 16:56:29 +0200

    Code quality improvements and minor visual fixes
    Changes:
    - Added Header to to each study view and made them use same style.
    - Renamed study-overview URL to match new study URL as intended.
    - Removed unused Sensors.js file from the game.
    - Removed some trash code...
    - Navbar code is now in navbar template rather than having duplicate code in each file.
    - Visual style improvements everywhere to make code more readable.

--- collections/Studies.js
+++ collections/Studies.js
@@ -1,7 +1,6 @@
 try{
 Studies = new Mongo.Collection('studies');
 
-
 Studies.allow({
   insert: function(userId, doc) {
     return !!userId;
@@ -10,8 +9,8 @@ Studies.allow({
     return !!userId;
   }
 });
- 
- // For Checkbox, Radio & Quick Answer Options
+
+// For Checkbox, Radio & Quick Answer Options
 Option = new SimpleSchema({
   option: {
     type: String,
@@ -21,7 +20,6 @@ Option = new SimpleSchema({
 });
 
 Question = new SimpleSchema({
-
 	question: {
 		type: String,
 		label: "Question"
@@ -39,7 +37,7 @@ Question = new SimpleSchema({
     	min: 0
     },
 
-    type: {
+  type: {
     type: Number,
     label: "Question type",
     optional: true,
@@ -85,7 +83,6 @@ Question = new SimpleSchema({
   },
 
   // Likeart Scale can also use same Max Value and Step Size
-
   maxLabel: {
   	type: String,
     optional: true,
@@ -110,7 +107,7 @@ Question = new SimpleSchema({
   	label: "Step Size"
   },
 
-  scaleStart:{
+  scaleStart: {
     type: Number,
     optional: true,
     label: "Scale Start"
@@ -121,13 +118,16 @@ Question = new SimpleSchema({
 Sensor = new SimpleSchema({
   sensorType: {
     type: String,
-    allowedValues: ["Accelerometer","Application", "Barometer","Battery","Bluetooth","Communication","Gravity", "Gyroscope","Installations",
-    "Light","Linear Accelerometer", "Location", "Magnetometer","Network","Processer","Proximity","Rotation",
-    "Screen","Telephony","Temperature","Wi-Fi"],
+    allowedValues: [
+        "Accelerometer", "Application", "Barometer", "Battery", "Bluetooth", "Communication", "Gravity", "Gyroscope",
+        "Installations", "Light", "Linear Accelerometer", "Location", "Magnetometer", "Network", "Processer",
+        "Proximity","Rotation", "Screen", "Telephony", "Temperature", "Wi-Fi"
+    ],
     autoform: {
       type: "hidden"
     }
   },
+
   sensorActive: {
     type: Boolean,
     autoform:{
@@ -138,103 +138,113 @@ Sensor = new SimpleSchema({
       label:false
     }
   },
+
   frequency: {
     type: String,
     label: "Frequency (in microsends)",
     optional: true
   },
+
   application: {
     type: [String],
     label: "Application Options",
     optional: true,
     autoform: {
       type: "select-checkbox",
-          options: function () {
-            return [
-              {label: "Status Notifications", value: "notification"},{label: "Status Crashes", value: "crash"},
-              {label: "Status keyboard", value: "keyboard"}
-            ];
-        }
+      options: function () {
+        return [
+          {label: "Status Notifications", value: "notification"},
+          {label: "Status Crashes", value: "crash"},
+          {label: "Status keyboard", value: "keyboard"}
+        ];
+      }
     }
   },
+
   communication: {
     type: [String],
     optional: true,
     label: "Communication Options",
     autoform: {
       type: "select-checkbox",
-          options: function () {
-            return [
-              {label: "Status Calls", value: "calls"},{label: "Status Messages", value: "messages"}
-            ];
-        }
+      options: function () {
+        return [
+          {label: "Status Calls", value: "calls"},
+          {label: "Status Messages", value: "messages"}
+        ];
+      }
     }
   },
+
   network: {
     type: [String],
     optional: true,
     label: "Network Options",
     autoform: {
       type: "select-checkbox",
-          options: function () {
-            return [
-              {label: "Status Network Events", value: "network"},{label: "Status Network Traffic", value: "traffic"}
-            ];
-        }
+      options: function () {
+        return [
+          {label: "Status Network Events", value: "network"},
+          {label: "Status Network Traffic", value: "traffic"}
+        ];
+      }
     }
   }
 });
 
 
 Schedule = new SimpleSchema({
-
-	hours: {
-		type: [Number],
-		optional: true,
+  hours: {
+    type: [Number],
+    optional: true,
     label: "Hour",
     autoform: {
       type: "select-checkbox-inline",
       label:false,
-          options: function () {
-            return [
-              {label: "00:00", value: 0},{label: "01:00", value: 1},{label: "02:00", value: 2},{label: "04:00", value: 4},
-              {label: "05:00", value: 5},{label: "06:00", value: 6},{label: "07:00", value: 7},{label: "08:00", value: 8},
-              {label: "09:00", value: 9},{label: "10:00", value: 10},{label: "11:00", value: 11},{label: "12:00", value: 12},
-              {label: "13:00", value: 13},{label: "14:00", value: 14},{label: "15:00", value: 15},{label: "16:00", value: 16},
-              {label: "17:00", value: 17},{label: "18:00", value: 18},{label: "19:00", value: 19},{label: "20:00", value: 20},
-              {label: "21:00", value: 21},{label: "22:00", value: 22},{label: "23:00", value: 23}
-            ];
-        }
+      options: function () {
+        return [
+          {label: "00:00", value: 0},{label: "01:00", value: 1},{label: "02:00", value: 2},{label: "04:00", value: 4},
+          {label: "05:00", value: 5},{label: "06:00", value: 6},{label: "07:00", value: 7},{label: "08:00", value: 8},
+          {label: "09:00", value: 9},{label: "10:00", value: 10},{label: "11:00", value: 11},{label: "12:00", value: 12},
+          {label: "13:00", value: 13},{label: "14:00", value: 14},{label: "15:00", value: 15},{label: "16:00", value: 16},
+          {label: "17:00", value: 17},{label: "18:00", value: 18},{label: "19:00", value: 19},{label: "20:00", value: 20},
+          {label: "21:00", value: 21},{label: "22:00", value: 22},{label: "23:00", value: 23}
+        ];
+      }
     }
-	},
+  },
 
-	weekdays: {
-		type: [String],
-		optional: true,
+  weekdays: {
+    type: [String],
+    optional: true,
     label: "Weekday",
     autoform: {
       type: "select-checkbox-inline",
       label:false,
-          options: function () {
-            return [
-              {label: "Monday", value: "monday"},{label: "Tuesday", value: "tuesday"},{label: "Wednesday", value: "wednesday"},
-              {label: "Thursday", value: "thursday"},{label: "Friday", value: "friday"},
-              {label: "Saturday", value: "saturday"},{label: "Sunday", value: "sunday"}
-            ];
-        }
-    }
-	},
-
-  questionSchedule: {
-    type: [Number],
-    label:"Select schedule for Questions",
-    optional: true,
-    autoform:{
-      label:false
+      options: function () {
+        return [
+          {label: "Monday", value: "monday"},
+          {label: "Tuesday", value: "tuesday"},
+          {label: "Wednesday", value: "wednesday"},
+          {label: "Thursday", value: "thursday"},
+          {label: "Friday", value: "friday"},
+          {label: "Saturday", value: "saturday"},
+          {label: "Sunday", value: "sunday"}
+        ];
+      }
     }
   },
 
-	scheduleCheck: {
+  questionSchedule: {
+    type: [Number],
+    label: "Select schedule for Questions",
+    optional: true,
+    autoform: {
+      label: false
+    }
+  },
+
+  scheduleCheck: {
     type: String,
     optional: true,
     autoform:{
@@ -245,15 +255,15 @@ Schedule = new SimpleSchema({
           {label: "Interval", value: "interval"},
           {label: "Time", value: "time"}
         ];
+      }
     }
-  }
-},
+  },
 
-	interval:{
-		type: Number,
-		label: "Interval",
-		optional: true
-	}
+  interval:{
+    type: Number,
+    label: "Interval",
+    optional: true
+  }
 
 });
 
@@ -287,7 +297,7 @@ Studies.attachSchema(new SimpleSchema({
       type: "boolean-radios",
       trueLabel: "Yes",
       falseLabel: "No",
-      defaultValue: false,
+      defaultValue: true,
     }
   },
 
@@ -299,7 +309,7 @@ Studies.attachSchema(new SimpleSchema({
   sensorCheck: {
     type: Boolean,
     label:"Do you want to enable the Sensors?",
-	optional: true,
+	  optional: true,
     autoform:{
       type: "boolean-radios",
       trueLabel: "Enabled",
@@ -320,29 +330,29 @@ Studies.attachSchema(new SimpleSchema({
   },
 
   start_date: {
-  	type: Date,
-  	label: "Start_date",
-  	optional: true,
+    type: Date,
+    label: "Start_date",
+    optional: true,
     autoform: {
-        type: "bootstrap-datepicker",
-        datePickerOptions:
-                {
-                    format: "mm-dd-yyyy"
-                }
+      type: "bootstrap-datepicker",
+      datePickerOptions: {
+        format: "mm-dd-yyyy"
       }
+    }
   },
+
   end_date: {
-  	type: Date,
-  	label: "End_date",
-  	optional: true,
+    type: Date,
+    label: "End_date",
+    optional: true,
     autoform: {
-        type: "bootstrap-datepicker",
-        datePickerOptions:
-                {
-                    format: "mm-dd-yyyy"
-                }
+      type: "bootstrap-datepicker",
+      datePickerOptions: {
+        format: "mm-dd-yyyy"
       }
+    }
   },
+
   createdAt: {
     type: Date,
     label: "Created at",
@@ -353,10 +363,11 @@ Studies.attachSchema(new SimpleSchema({
       type: "hidden"
     }
   },
+
   exported: {
     type: Boolean,
     label: "Exported",
-	  optional: true,
+    optional: true,
     defaultValue: false,
     autoform: {
       type: "hidden"
--- server/init.js
+++ server/init.js
@@ -1,18 +1,4 @@
 Meteor.startup(function() {
-  // Populate Sensors collection if empty
-  if (Sensors.find().count() === 0) {
-    Sensors.insert({
-      type: 'LIGHT'
-    });
-    Sensors.insert({
-      type: 'ACCELEROMETER'
-    });
-    Sensors.insert({
-      type: 'GRAVITY'
-    });
-    Sensors.insert({
-      type: 'BAROMETER'
-    });
-  }
+  
 });
 
